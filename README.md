9380447a55cd691379e4f70e514218d4143f28e5
LucaMenestrina <menestrina.luca@gmail.com>
2021-03-10 12:40:05 +0100

    bioRxive

--- README.md
+++ README.md
@@ -4,4 +4,72 @@
   </a>
 </p>
 
-The COVID-19 pandemic poses a huge problem of public health that requires the implementation of all available means to contrast it, and drugs are one of them. In this context, we observed an unmet need of depicting the continuously evolving scenario of the ongoing drug clinical trials through an easy-to-use, freely accessible online tool. Starting from this consideration, we developed [COVIDrugNet](http://compmedchem.unibo.it/covidrugnet), a web application that allows users to capture a holistic view and keep up to date on how the clinical drug research is responding to the SARS-CoV-2 infection.
+The COVID-19 pandemic poses a huge problem of public health that requires the implementation of all available means to contrast it, and drugs are one of them. In this context, we observed an unmet need of depicting the continuously evolving scenario of the ongoing drug clinical trials through an easy-to-use, freely accessible online tool. Starting from this consideration, we developed [**COVIDrugNet**](http://compmedchem.unibo.it/covidrugnet), a web application that allows users to capture a holistic view and keep up to date on how the clinical drug research is responding to the SARS-CoV-2 infection.
+
+In [our article](https://www.biorxiv.org/content/early/2021/03/09/2021.03.05.433897), we describe the web app and show through some examples how one can explore the whole landscape of medicines in clinical trial for the treatment of COVID-19 and try to probe the consistency of the current approaches with the available biological and pharmacological evidence. We conclude that careful analyses of the COVID-19 drug-target system based on COVIDrugNet can help to understand the biological implications of the proposed drug options, and eventually improve the search for more effective therapies.
+
+### Cite
+
+Please cite [our paper](https://www.biorxiv.org/content/early/2021/03/09/2021.03.05.433897) (and the respective papers of the methods used) if you use *COVIDrugNet* in your own work:
+
+```
+@article {Menestrina2021.03.05.433897,
+	 title = {COVIDrugNet: a network-based web tool to investigate the drugs currently in clinical trial to contrast COVID-19},
+	 author = {Menestrina, Luca and Cabrelle, Chiara and Recanatini, Maurizio},
+	 journal = {bioRxiv}
+	 year = {2021},
+	 doi = {10.1101/2021.03.05.433897},
+	 URL = {https://www.biorxiv.org/content/early/2021/03/09/2021.03.05.433897},
+	 publisher = {Cold Spring Harbor Laboratory},
+}
+```
+
+### Data Sources
+
+- [DrugBank](https://go.drugbank.com/)
+- [STRING](https://string-db.org/)
+- [DisGeNET](https://www.disgenet.org/)
+- [SWISS-MODEL](https://swissmodel.expasy.org/)
+- [RCSB PDB](https://www.rcsb.org/)
+- [UniProt](https://www.uniprot.org/)
+- [ChEMBL](https://www.ebi.ac.uk/chembl/)
+- [Gordon et al. Virus-Host interactome](https://doi.org/10.1038/s41586-020-2286-9)
+- [Chen et al. Virus-Host interactome](http://127.0.0.1:8050/covidrugnet/10.1101/2020.12.31.424961)
+
+### Browser compatibility
+The website has been tested on the following browsers and OSs:
+
+|             | Google Chrome | Firefox | Safari | Microsoft Edge |
+|-------------|-------|--------|---------|---------|
+| **Linux**   | ✅    | ✅     | ?      | ?     |
+| **Windows** | ✅    | ✅     | ?      | ✅      |
+| **macOS**   | ✅    | ✅       | ✅        | ?        |
+
+
+### Dependencies
+
+COVIDrugNet is entirely written in Python 3 and would not be possible without the following packages:
+
+- [beautifulsoup4](https://www.crummy.com/software/BeautifulSoup/)
+- [chembl_webresource_client](https://github.com/chembl/chembl_webresource_client)
+- [dash](https://plotly.com/dash/)
+- [dash_bootstrap_components](https://dash-bootstrap-components.opensource.faculty.ai/)
+- [dash_core_components](https://dash.plotly.com/dash-core-components)
+- [dash_cytoscape](https://dash.plotly.com/cytoscape)
+- [dash_daq](https://dash.plotly.com/dash-daq)
+- [dash_html_components](https://dash.plotly.com/dash-html-components)
+- [dash_table](https://github.com/plotly/dash-table)
+- [matplotlib](https://matplotlib.org/)
+- [networkx](https://networkx.org/)
+- [numpy](https://numpy.org/)
+- [pandas](https://pandas.pydata.org/)
+- [plotly](https://plotly.com/)
+- [powerlaw](https://github.com/jeffalstott/powerlaw)
+- [PubChemPy](https://pubchempy.readthedocs.io/)
+- [pygraphviz](https://pygraphviz.github.io/)
+- [scikit](https://scikit-learn.org/)
+- [scipy](https://www.scipy.org/)
+- [rdkit](https://www.rdkit.org/)
+- [requests](https://requests.readthedocs.io/en/master/)
+- [tqdm](https://github.com/tqdm/tqdm)
+- [visdcc](https://github.com/jimmybow/visdcc)
--- pages/about.py
+++ pages/about.py
@@ -19,14 +19,14 @@ layout=dbc.Col([
                         dbc.Col(
                             html.A(
                                 dbc.Row([
-                                    #da controllare se abbiamo i diritti per farlo e soprattutto va aggiornata a quella del mese corretto
-                                    dbc.Col([html.Img(src=app.get_asset_url("imgs/wordcloud.png"), style={"width":"100%"})], align="center", sm=5, lg=4),#imgs/journal_cover.svg
+                                    dbc.Col([html.Img(src=app.get_asset_url("imgs/wordcloud.png"), style={"width":"100%"})], align="center", sm=5, lg=4),
                                     dbc.Col([
                                         html.Br(),
                                         html.H4(html.Strong("COVIDrugNet: a network-based web tool to investigate the drugs currently in clinical trial to contrast COVID-19"), style={"margin-bottom":"1rem"}),
                                         html.H5("Luca Menestrina, Chiara Cabrelle and Maurizio Recanatini"),
                                         html.P(html.Em("Department of Pharmacy and Biotechnology, Alma Mater Studiorum - University of Bologna 40126 Bologna, Italy"), style={"margin-bottom":"1rem"}),
                                         html.Br(),
+                                        html.H5("Preprint at bioRxiv"),
                                         # html.H5("Jurnal, Pages, Issue, Year ..."),
                                         # html.Br(),
                                         # dbc.Row([
@@ -43,28 +43,44 @@ layout=dbc.Col([
                                         #     #     html.P(html.Time("Not Yet",dateTime="1900-01-01"), style={"color":"grey"}),
                                         #     # ], align="center")
                                         # ], justify="around", align="center"),
-                                        # html.Br(),
-                                        # html.H5("DOI"),
-                                        # html.A("doi", href="", target="_blank"),
+                                        html.Br(),
+                                        html.H5("DOI:"),
+                                        html.A("https://doi.org/10.1101/2021.03.05.433897", href="https://doi.org/10.1101/2021.03.05.433897", target="_blank"),
                                     ], align="center", sm=7, lg=8)
                                 ], justify="center", align="center", id="paper"),
-                            href="", target="_blank", style={"color":"black", "text-decoration":"none"}),
+                            href="https://www.biorxiv.org/content/10.1101/2021.03.05.433897v1", target="_blank", style={"color":"black", "text-decoration":"none"}),
                         align="center", sm=10, lg=7, style={"padding-right":"2vw"}),
                         html.Br(),
                         dbc.Col([
                             html.Br(),
                             html.H4(html.Strong("Abstract")),
                             html.P("The COVID-19 pandemic poses a huge problem of public health that requires the implementation of all available means to contrast it, and drugs are one of them. In this context, we observed an unmet need of depicting the continuously evolving scenario of the ongoing drug clinical trials through an easy-to-use, freely accessible online tool. Starting from this consideration, we developed COVIDrugNet, a web application that allows users to capture a holistic view and keep up to date on how the clinical drug research is responding to the SARS-CoV-2 infection.", style={"margin-bottom":0}),
-                            html.P("Here, we describe the web app and show through some examples how one can explore the whole landscape of medicines in clinical trial for the treatment of COVID-19 and try to probe the consistency of the current approaches with the available biological and pharmacological evidence. We conclude that careful analyses of the COVID-19 drug-target system based on COVIDrugNet can help to understand the biological implications of the proposed drug options, and eventually improve the search for more effective therapies."),
+                            html.P([
+                                html.A("In the paper", href="https://www.biorxiv.org/content/10.1101/2021.03.05.433897v1", target="_blank", style={"color":"black", "text-decoration":"none"}),
+                                " we describe the web app and show through some examples how one can explore the whole landscape of medicines in clinical trial for the treatment of COVID-19 and try to probe the consistency of the current approaches with the available biological and pharmacological evidence. We conclude that careful analyses of the COVID-19 drug-target system based on COVIDrugNet can help to understand the biological implications of the proposed drug options, and eventually improve the search for more effective therapies."
+                            ]),
                         ], align="center", sm=10, lg=5, style={"text-align":"justify", "font-size":"large", "padding-right":"1vw"})
                     ], justify="around", align="center"),
                     html.Br(),
-                    # html.Br(),
-                    # html.H4("Citing COVIDrugNet:"),
-                    # html.Br(),
-                    # html.H5(["If you are using data from ", html.Em("COVIDrugNet"), " please cite the corresponding article:"]),
+                    html.Br(),
+                    html.Br(),
+                    html.H4("Citing COVIDrugNet"),
+                    html.Br(),
+                    html.H5(["If you are using data from ", html.Em("COVIDrugNet"), " please cite ", html.A("our article", href="https://www.biorxiv.org/content/10.1101/2021.03.05.433897v1", target="_blank", style={"color":"black"}),":"]),
+                    html.Br(),
+                    html.Pre([
+                        "@article {Menestrina2021.03.05.433897,",html.Br(),
+                        "\t title = {COVIDrugNet: a network-based web tool to investigate the drugs currently in clinical trial to contrast COVID-19},",html.Br(),
+                        "\t author = {Menestrina, Luca and Cabrelle, Chiara and Recanatini, Maurizio},",html.Br(),
+                        "\t journal = {bioRxiv}",html.Br(),
+                        "\t year = {2021},",html.Br(),
+                        "\t doi = {10.1101/2021.03.05.433897},",html.Br(),
+                        "\t URL = {https://www.biorxiv.org/content/early/2021/03/09/2021.03.05.433897},",html.Br(),
+                        "\t publisher = {Cold Spring Harbor Laboratory},",html.Br(),
+                        "}"
+                    ], style={"background-color":"rgba(211,211,211,0.15)", "border-radius": "0.6rem"}),
                     # html.P("bib string for easy citing, come per gli altri sotto"),
-                    # html.Br(),
+                    html.Br(),
                     html.Hr(),
                     html.Br(),
                     html.H4(["Last Database Update: ",html.Time(["14",html.Sup("th")," January 2021"], dateTime="2021-01-14")]),
@@ -85,7 +101,7 @@ layout=dbc.Col([
                             html.A(html.P(["Szklarczyk, Damian et al. “STRING v11: protein-protein association networks with increased coverage, supporting functional discovery in genome-wide experimental datasets.” ",html.Em("Nucleic acids research")," vol. 47,D1 (2019): D607-D613"]), href="https://doi.org/10.1093/nar/gky1131", target="_blank", style={"color":"black"})
                         ], style={"background":"url(https://string-db.org/images/favicon.png", "background-size":"2rem", "background-repeat":"no-repeat","background-position":"left", "padding-left":"4rem"}),
                         html.Li([
-                            html.A(html.Strong("DisGeNet"), href="https://www.disgenet.org/", target="_blank", style={"color":"black"}),
+                            html.A(html.Strong("DisGeNET"), href="https://www.disgenet.org/", target="_blank", style={"color":"black"}),
                             html.A(html.P(["Piñero, Janet et al. “The DisGeNET knowledge platform for disease genomics: 2019 update.” ",html.Em("Nucleic acids research")," vol. 48,D1 (2020): D845-D855"]), href="https://doi.org/10.1093/nar/gkz1021", target="_blank", style={"color":"black"})
                         ], style={"background":"url(https://www.disgenet.org/static/disgenet_ap1/images/logo_disgenet.png)", "background-size":"3rem", "background-repeat":"no-repeat","background-position":"left", "padding-left":"4rem"}),
                         html.Li([
@@ -116,27 +132,43 @@ layout=dbc.Col([
                     html.Br(),
                     html.Hr(),
                     html.Br(),
-                    html.H3("COVID-19 Drugs Networker would not be possible without the following packages:"),
+                    html.H3("COVIDrugNet would not be possible without the following packages:"),
                     html.Br(),
                     html.Ul([
                         html.Li([
                             html.A("Python 3", href="https://www.python.org/", target="_blank", style={"color":"black"}),
                             html.Ul([
-                                html.Li(html.A("pandas", href="https://pandas.pydata.org/", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("numpy", href="https://numpy.org/", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("os", href="https://docs.python.org/3/library/os.html", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("pickle", href="https://docs.python.org/3/library/pickle.html", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("json", href="https://docs.python.org/3/library/json.html", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("itertools", href="https://docs.python.org/3/library/itertools.html", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("requests", href="https://requests.readthedocs.io/en/master/", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("time", href="https://docs.python.org/3/library/time.html", target="_blank", style={"color":"black"})),
                                 html.Li(html.A("datetime", href="https://docs.python.org/3/library/datetime.html", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("webbrowser", href="https://docs.python.org/3/library/webbrowser.html", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("threading", href="https://docs.python.org/3/library/threading.html", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("itertools", href="https://docs.python.org/3/library/itertools.html", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("json", href="https://docs.python.org/3/library/json.html", target="_blank", style={"color":"black"})),
                                 html.Li([
                                     html.A("matplotlib pyplot", href="https://matplotlib.org/", target="_blank", style={"color":"black"}),
                                     #https://ieeexplore.ieee.org/document/4160265
                                 ]),
+                                html.Li(html.A("numpy", href="https://numpy.org/", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("os", href="https://docs.python.org/3/library/os.html", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("pandas", href="https://pandas.pydata.org/", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("pickle", href="https://docs.python.org/3/library/pickle.html", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("requests", href="https://requests.readthedocs.io/en/master/", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("threading", href="https://docs.python.org/3/library/threading.html", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("time", href="https://docs.python.org/3/library/time.html", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("webbrowser", href="https://docs.python.org/3/library/webbrowser.html", target="_blank", style={"color":"black"})),
+                            ])
+                        ]),
+                        html.Li(html.A("beautiful soup", href="https://www.crummy.com/software/BeautifulSoup/", target="_blank", style={"color":"black"})),
+                        html.Li([
+                            html.A("chembl webresource client", href="https://github.com/chembl/chembl_webresource_client", target="_blank", style={"color":"black"}),
+                            #https://github.com/chembl/chembl_webresource_client
+                        ]),
+                        html.Li([
+                            html.A("dash", href="https://plotly.com/dash/", target="_blank", style={"color":"black"}),
+                            html.Ul([
+                                html.Li(html.A("dash bootstrap components", href="https://dash-bootstrap-components.opensource.faculty.ai/", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("dash core components", href="https://dash.plotly.com/dash-core-components", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("dash cytoscape", href="https://dash.plotly.com/cytoscape", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("dash daq", href="https://dash.plotly.com/dash-daq", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("dash html components", href="https://dash.plotly.com/dash-html-components", target="_blank", style={"color":"black"})),
+                                html.Li(html.A("dash table", href="https://github.com/plotly/dash-table", target="_blank", style={"color":"black"})),
                             ])
                         ]),
                         html.Li([
@@ -147,23 +179,10 @@ layout=dbc.Col([
                             html.A("plotly", href="https://plotly.com/", target="_blank", style={"color":"black"}),
                             #https://plotly.com/chart-studio-help/citations/
                         ]),
-                        html.Li([
-                            html.A("dash", href="https://plotly.com/dash/", target="_blank", style={"color":"black"}),
-                            html.Ul([
-                                html.Li(html.A("dash core components", href="https://dash.plotly.com/dash-core-components", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("dash html components", href="https://dash.plotly.com/dash-html-components", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("dash bootstrap components", href="https://dash-bootstrap-components.opensource.faculty.ai/", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("dash cytoscape", href="https://dash.plotly.com/cytoscape", target="_blank", style={"color":"black"})),
-                                html.Li(html.A("dash daq", href="https://dash.plotly.com/dash-daq", target="_blank", style={"color":"black"})),
-                            ])
-                        ]),
+
                         html.Li(html.A("powerlaw", href="https://github.com/jeffalstott/powerlaw", target="_blank", style={"color":"black"})),
                         html.Li(html.A("PubChemPy", href="https://pubchempy.readthedocs.io/", target="_blank", style={"color":"black"})),
-                        html.Li([
-                            html.A("chembl webresource client", href="https://github.com/chembl/chembl_webresource_client", target="_blank", style={"color":"black"}),
-                            #https://github.com/chembl/chembl_webresource_client
-                        ]),
-                        html.Li(html.A("beautiful soup", href="https://www.crummy.com/software/BeautifulSoup/", target="_blank", style={"color":"black"})),
+                        html.Li(html.A("pygraphviz", href="https://pygraphviz.github.io/", target="_blank", style={"color":"black"})),
                         html.Li([
                             html.A("rdkit", href="https://www.rdkit.org/", target="_blank", style={"color":"black"}),
                             #https://rdkit-discuss.narkive.com/9QGX4Vxh/is-there-a-way-to-cite-rdkit-in-a-paper
@@ -172,7 +191,9 @@ layout=dbc.Col([
                             html.A("scikit-learn", href="https://scikit-learn.org/", target="_blank", style={"color":"black"}),
                             #https://scikit-learn.org/stable/about.html#citing-scikit-learn
                         ]),
+                        html.Li(html.A("scipy", href="https://www.scipy.org/", target="_blank", style={"color":"black"})),
                         html.Li(html.A("tqdm", href="https://github.com/tqdm/tqdm", target="_blank", style={"color":"black"})),
+
                         html.Li(html.A("visdcc", href="https://github.com/jimmybow/visdcc", target="_blank", style={"color":"black"})),
                     ]),
 
