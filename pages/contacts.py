733eb8e4cf4fcfad06b7776067e5a87e9ee92ddb
LucaMenestrina <menestrina.luca@gmail.com>
2022-05-31 12:48:47 +0200

    fix dcc/html deprecated (contact page)

--- pages/contacts.py
+++ pages/contacts.py
@@ -1,9 +1,7 @@
-# import dash_core_components as dcc # deprecated
-from dash import dcc
+import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 
-# import dash_html_components as html  # deprecated
-from dash import html
+import dash_html_components as html
 
 from building_blocks import *
 from callbacks import *
